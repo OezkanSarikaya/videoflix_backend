6c2a30b7a30ad2f3e5af0d89994d5bc8c3b6f8cd
OezkanSarikaya <oezkan.sarikaya@freenet.de>
2025-01-07 12:30:28 +0100

    Add demo media files, custom user model and custom admin form

--- videoflix_app/admin.py
+++ videoflix_app/admin.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
 from .models import Video, Genre
+from import_export.admin import ImportExportModelAdmin
+from import_export import resources
 
 # Register your models here.
 
-admin.site.register(Video)
+class VideoResource(resources.ModelResource):
+
+    class Meta:
+        model = Video  
+
+@admin.register(Video)
+class VideoAdmin(ImportExportModelAdmin):
+    pass
+
 admin.site.register(Genre)
