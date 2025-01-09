7486cc569d9bcf80d94803cc528369c0f8183fbe
OezkanSarikaya <oezkan.sarikaya@freenet.de>
2025-01-09 13:19:17 +0100

    setup password reset function

--- users/tasks.py
+++ users/tasks.py
@@ -3,9 +3,19 @@ from django.conf import settings
 from django.urls import reverse
 
 def send_activation_email(user, uid, token):
-    activation_link = f"http://127.0.0.1:8000/api/users/activate/{uid}/{token}/"
+    # activation_link = f"http://localhost:4200/activate/{uid}/{token}/"
+    activation_link = f"http://localhost:4200/activate/?uid={uid}&token={token}"
     # activation_link = f"{settings.FRONTEND_URL}/activate/{uid}/{token}/"
     subject = "Activate your account"
     message = f"Hi {user.email},\n\nPlease click the link below to activate your account:\n{activation_link}"
     
     send_mail(subject, message, settings.DEFAULT_FROM_EMAIL, [user.email])
+
+# def send_password_reset_email(user, uid, token):
+#     # activation_link = f"http://localhost:4200/activate/{uid}/{token}/"
+#     activation_link = f"http://localhost:4200/activate/?uid={uid}&token={token}"
+#     # activation_link = f"{settings.FRONTEND_URL}/activate/{uid}/{token}/"
+#     subject = "Reset your passwort"
+#     message = f"Hi {user.email},\n\nPlease click the link below to reset your password:\n{activation_link}"
+    
+#     send_mail(subject, message, settings.DEFAULT_FROM_EMAIL, [user.email])
