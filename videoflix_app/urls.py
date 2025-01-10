05cd8124c40a0bba6a690e483e6218615ee50505
OezkanSarikaya <oezkan.sarikaya@freenet.de>
2025-01-10 17:13:54 +0100

    Add new Endpoint for Videodetails

--- videoflix_app/urls.py
+++ videoflix_app/urls.py
@@ -1,8 +1,9 @@
 # videoflix_app/urls.py
 from django.urls import path
-from .views import VideoListView, GenreVideoListView
+from .views import VideoListView, GenreVideoListView, VideoDetailView
 
 urlpatterns = [
     path('videos/', VideoListView.as_view(), name='video_list'),  # Liste der Videos
+    path('videos/<int:id>/', VideoDetailView.as_view(), name='video_detail'),
     path('genres/videos/', GenreVideoListView.as_view(), name='genre_video_list'),
 ]
--- videoflix_app/views.py
+++ videoflix_app/views.py
@@ -8,11 +8,30 @@ from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from .models import Video, Genre
 from .serializers import VideoSerializer, GenreWithVideosSerializer
+from rest_framework import status
 
 class VideoListView(generics.ListAPIView):
     queryset = Video.objects.all()
     serializer_class = VideoSerializer
     permission_classes = [IsAuthenticated]  # Nur authentifizierte Benutzer dürfen zugreifen
+
+class VideoDetailView(APIView):
+    permission_classes = [IsAuthenticated] 
+    def get(self, request, id, format=None):
+        try:
+            video = Video.objects.get(id=id)  # Hole Video anhand der ID
+            # Hier die Daten aus dem Video-Objekt formatieren
+            data = {
+                'id': video.id,
+                'title': video.title,
+                'description': video.description,
+                'video_file': video.video_file.url,
+                'thumbnail': video.thumbnail.url,
+                'created_at': video.created_at,
+            }
+            return Response(data)
+        except Video.DoesNotExist:
+            return Response({'error': 'Video not found'}, status=status.HTTP_404_NOT_FOUND)
 
 class GenreVideoListView(APIView):
     def get(self, request):
