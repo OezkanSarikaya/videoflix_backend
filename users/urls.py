a31dd13bf5f22427a65d9015c1800b1f5508f017
OezkanSarikaya <oezkan.sarikaya@freenet.de>
2025-01-10 08:52:59 +0100

    update endpoint and serializer for videos

--- users/urls.py
+++ users/urls.py
@@ -10,12 +10,9 @@ urlpatterns = [
     path('signup/', UserRegistrationView.as_view(), name='user_signup'),
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-
     path('password_reset/', PasswordResetRequestView.as_view(),
          name='password_reset'),
     path('password_reset/confirm/<uidb64>/<token>/',
          PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-
     path('activate/<uidb64>/<token>/', activate_account, name='account-activate'),
-
 ]
--- videoflix_app/views.py
+++ videoflix_app/views.py
@@ -3,12 +3,20 @@
 # Create your views here.
 # videoflix_app/views.py
 from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
-from .models import Video
-from .serializers import VideoSerializer
+from .models import Video, Genre
+from .serializers import VideoSerializer, GenreWithVideosSerializer
 
 class VideoListView(generics.ListAPIView):
     queryset = Video.objects.all()
     serializer_class = VideoSerializer
     permission_classes = [IsAuthenticated]  # Nur authentifizierte Benutzer dürfen zugreifen
+
+class GenreVideoListView(APIView):
+    def get(self, request):
+        genres = Genre.objects.prefetch_related('videos').all()
+        serializer = GenreWithVideosSerializer(genres, many=True)
+        return Response(serializer.data)
 
