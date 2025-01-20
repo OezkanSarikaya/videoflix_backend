227dd88ac4e7f3df8a2f9de7b8eb4fabf86ae628
OezkanSarikaya <oezkan.sarikaya@freenet.de>
2025-01-20 11:43:07 +0100

    Clean up Code

--- users/tasks.py
+++ users/tasks.py
@@ -2,20 +2,10 @@ from django.core.mail import send_mail
 from django.conf import settings
 from django.urls import reverse
 
+
 def send_activation_email(user, uid, token):
-    # activation_link = f"http://localhost:4200/activate/{uid}/{token}/"
     activation_link = f"http://localhost:4200/activate/?uid={uid}&token={token}"
     # activation_link = f"{settings.FRONTEND_URL}/activate/{uid}/{token}/"
     subject = "Activate your account"
     message = f"Hi {user.email},\n\nPlease click the link below to activate your account:\n{activation_link}"
-    
     send_mail(subject, message, settings.DEFAULT_FROM_EMAIL, [user.email])
-
-# def send_password_reset_email(user, uid, token):
-#     # activation_link = f"http://localhost:4200/activate/{uid}/{token}/"
-#     activation_link = f"http://localhost:4200/activate/?uid={uid}&token={token}"
-#     # activation_link = f"{settings.FRONTEND_URL}/activate/{uid}/{token}/"
-#     subject = "Reset your passwort"
-#     message = f"Hi {user.email},\n\nPlease click the link below to reset your password:\n{activation_link}"
-    
-#     send_mail(subject, message, settings.DEFAULT_FROM_EMAIL, [user.email])
--- videoflix_app/serializers.py
+++ videoflix_app/serializers.py
@@ -1,19 +1,22 @@
 from rest_framework import serializers
 from .models import Video, Genre, VideoProgress
 
+
 class VideoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Video
-        fields = ['id', 'title', 'description', 'video_file', 'thumbnail', 'created_at']
+        fields = ["id", "title", "description", "video_file", "thumbnail", "created_at"]
+
 
 class GenreWithVideosSerializer(serializers.ModelSerializer):
     videos = VideoSerializer(many=True)
 
     class Meta:
         model = Genre
-        fields = ['title', 'videos']  
+        fields = ["title", "videos"]
+
 
 class VideoProgressSerializer(serializers.ModelSerializer):
     class Meta:
         model = VideoProgress
-        fields = ['user', 'video', 'progress', 'updated_at']      
+        fields = ["user", "video", "progress", "updated_at"]
