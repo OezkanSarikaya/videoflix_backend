8c989f904ac1c820a1ce83df4fe67da87fbe5a7a
OezkanSarikaya <oezkan.sarikaya@freenet.de>
2025-02-12 12:30:05 +0100

    update settings.py

--- videoflix_project/settings.py
+++ videoflix_project/settings.py
@@ -16,6 +16,8 @@ from datetime import timedelta  # import this library top of the settings.py fil
 import os
 
 print(f"DEBUG value from environment: {os.getenv('DEBUG')}")
+
+
 # import psycopg2
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
