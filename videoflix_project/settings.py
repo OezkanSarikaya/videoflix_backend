abc2769ed50df95bbcadc8efd9ffb81d420aa119
OezkanSarikaya <oezkan.sarikaya@freenet.de>
2025-02-12 12:48:28 +0100

    Test: Deployment überprüfen

--- videoflix_project/settings.py
+++ videoflix_project/settings.py
@@ -15,7 +15,7 @@ from dotenv import load_dotenv
 from datetime import timedelta  # import this library top of the settings.py file
 import os
 
-print(f"DEBUG value from environment: {os.getenv('DEBUG')}")
+print(f"DEBUG Wert from environment: {os.getenv('DEBUG')}")
 
 
 # import psycopg2
